8b8728b20af390d4f235d1dedc6dd793a802afa6
Baboyma Kagniniwa <baboyma@users.noreply.github.com>
2023-02-13 14:56:12 -0500

    Input files - batch download.

---
+++ Scripts/01_data_extraction.R
@@ -0,0 +1,146 @@
+##  PROJECT: Quaterly Analytics ETL Process
+##  AUTHOR:  Baboyma Kagniniwa | USAID
+##  PURPOSE: Extract Input MSD files for hyperfile processing
+##  REF. ID: b51c7bd4
+##  LICENCE: MIT
+##  DATE:    2023-02-13
+##  UPDATE:  2023-02-13
+
+# LIBRARIES ----
+  
+  library(tidyverse)
+  library(glamr)
+  library(grabr)
+  library(here)
+  library(fs)
+
+# NOTES ----
+
+# DISCLAIMERS ----
+
+# GLOBAL PARAMS ----
+
+  ## Directories 
+
+  dir_mer <- si_path(type = "path_msd")
+  
+  dir_hypers <- paste0("MSDs-4HyprProcess-", curr_date())
+  
+  dir_hypers <- file.path(dir_mer, dir_hypers)
+  
+  dir_hnats <- file.path(dir_hypers, "NATs")
+  dir_hous <- file.path(dir_hypers, "OUs")
+  dir_hpsnus <- file.path(dir_hypers, "PSNUs")
+  
+  dir_hsites <- file.path(dir_hypers, "Sites")
+  dir_hsites_curr <- file.path(dir_hsites, "Current")
+  dir_hsites_prev <- file.path(dir_hsites, "Previous")
+  
+  if (!dir.exists(dir_hypers)) {
+    dir.create(dir_hypers)
+    
+    dir.create(dir_hnats)
+    dir.create(dir_hous)
+    dir.create(dir_hpsnus)
+    
+    dir.create(dir_hsites)
+    dir.create(dir_hsites_curr)
+    dir.create(dir_hsites_prev)
+  }
+  
+  #open_path(dir_hypers)
+  
+  # Pano Access
+  
+  user <- pano_user()
+  pass <- pano_pwd()
+  
+  sess <- pano_session(username = user, password = pass)
+  
+  url <- "https://pepfar-panorama.org/forms/downloads/"
+  
+# FUNCTIONS ----
+  
+# DATA EXTRACTION ----
+  
+  # Recent MSD Release 
+  
+  recent_fldr <- url %>%
+    pano_content(session = sess) %>%
+    pano_elements() %>%
+    dplyr::filter(stringr::str_detect(item, "^MER")) %>%
+    dplyr::pull(item)
+  
+  ## Identify Latest Release
+  curr_release <- stringr::str_extract(recent_fldr, "(?<=Q\\d{1}[:space:]).*")
+  curr_status <- base::ifelse(stringr::str_detect(recent_fldr, "Post|Clean"), "clean", "initial")
+  curr_fy <- stringr::str_extract(recent_fldr, "[:digit:]{4}") %>% as.numeric()
+  curr_qtr <- stringr::str_extract(recent_fldr, "(?<=Q)[:digit:]") %>% as.numeric()
+  
+  ## Extract Data items
+  items <- pano_extract(item = "mer",
+                        version = curr_status,
+                        fiscal_year = curr_fy,
+                        quarter = curr_qtr,
+                        username = user,
+                        password = pass,
+                        unpack = TRUE)
+  
+  ## Downloads zip files
+  
+  ## Nat / SUBNAT
+  
+  items %>% 
+    filter(type == "file zip_file",
+           parent == recent_fldr,
+           str_detect(item, ".*_NAT_SUBNAT_.*.zip$")) %>% 
+    pull(path) %>% 
+    pano_download(item_url = ., 
+                  session = sess, 
+                  dest = dir_hnats)
+  
+  ## OUs
+  
+  items %>% 
+    filter(type == "file zip_file",
+           str_detect(parent, paste0(recent_fldr, "$|", recent_fldr, "/FY")),
+           str_detect(item, ".*_OU_IM_FY.*.zip$")) %>% 
+    pull(path) %>%
+    walk(~pano_download(item_url = .x, 
+                        session = sess, 
+                        dest = dir_hous))
+  
+  ## PSNUs
+  
+  items %>% 
+    filter(type == "file zip_file",
+           str_detect(parent, paste0(recent_fldr, "$|", recent_fldr, "/FY")),
+           str_detect(item, ".*_PSNU_IM_FY.*.zip$")) %>% 
+    pull(path) %>%
+    walk(~pano_download(item_url = .x, 
+                        session = sess, 
+                        dest = dir_hpsnus))
+  
+  
+  ## Sites / Current
+  
+  items %>% 
+    filter(type == "file zip_file",
+           str_detect(parent, paste0(recent_fldr, "/Site Level$")),
+           str_detect(item, ".*_Site_IM_FY.*.zip$")) %>% 
+    pull(path) %>%
+    walk(~pano_download(item_url = .x, 
+                        session = sess, 
+                        dest = dir_hsites_curr))
+  
+  ## Sites / Previous
+  
+  items %>% 
+    filter(type == "file zip_file",
+           str_detect(parent, paste0(recent_fldr, "/FY.*/Site Level$")),
+           str_detect(item, ".*_Site_IM_FY.*.zip$")) %>% 
+    pull(path) %>%
+    walk(~pano_download(item_url = .x, 
+                        session = sess, 
+                        dest = dir_hsites_prev))
+  
